6bd9deea720d98b2ea315d944c5c7b2aeeb0ce38
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-08-02 23:32:09 -0500

    Router and gorilla mux

--- main.go
+++ main.go
@@ -4,16 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		fmt.Fprintf(w, "Hola mundo desde mi servidor web con GO")
-	})
+	router := mux.NewRouter().StrictSlash(true)
 	fmt.Println("El servidor esta corriendo en http://localhost:8080")
-	server := http.ListenAndServe(":8080", nil)
+
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contact", contact)
+
+	server := http.ListenAndServe(":8080", router)
 
 	log.Fatal(server)
 
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hola mundo desde mi servidor web con GO")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Esta es la pagina de contacto: 5777-324-334")
 }
